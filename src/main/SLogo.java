23d7e82902bbd6f103604e157bdeb7f501786148
karenli95 <kjl32@duke.edu>
2015-10-04 11:45:53 -0400

    Created View classes and Main class

---
+++ src/main/SLogo.java
@@ -0,0 +1,24 @@
+package main;
+
+import view.GUIManager;
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class SLogo extends Application {
+	
+	@Override
+	public void start(Stage stage) {
+		Screen screen = Screen.getPrimary();
+		Rectangle2D bounds = screen.getVisualBounds();
+		stage.setWidth(bounds.getWidth());
+		stage.setHeight(bounds.getHeight());
+		stage.setResizable(false);
+		new GUIManager(stage);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/view/GUIManager.java
@@ -0,0 +1,40 @@
+package view;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class GUIManager {
+	private static final String TITLE = "SLogo";
+
+	protected Stage myStage;
+	private Scene myScene;
+	private Group myRoot;
+	
+	private int myWindowWidth, myWindowHeight;
+
+	public GUIManager(Stage stage){
+		myStage = stage;
+		Scene scene = init((int)stage.getWidth(), (int)stage.getHeight());
+		stage.setScene(scene);
+		stage.setTitle(TITLE);
+		
+		stage.show();
+	}
+	
+	
+	/**
+	 * Initialize the window
+	 * @param width The width of the window
+	 * @param height The height of the window
+	 * @return the Scene that was initialized
+	 */
+	private Scene init(int width, int height) {
+		myWindowWidth = width;
+		myWindowHeight = height;
+		myRoot = new Group();
+		myScene = new Scene(myRoot,width,height,Color.AZURE);
+		return myScene;
+	}
+}
