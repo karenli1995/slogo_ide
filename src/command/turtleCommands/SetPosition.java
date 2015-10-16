7ffb17b05ad5322691fca9668803f0d30b3d9486
Kevin Wang <kw197@duke.edu>
2015-10-16 13:03:42 -0400

    bug fix

--- src/command/turtleCommands/SetPosition.java
+++ src/command/turtleCommands/SetPosition.java
@@ -23,7 +23,7 @@ public class SetPosition extends Command {
 	public Data execute(List<ParseTreeNode<Command>> newLocation, Data data) {
 
 		//this.setValue(moveTurtle.calculateDistanceBetweenTwoPoints(newLocation, data));
-		Point2D point = new Point2D(newLocation.get(0).getCommand().getValue(), newLocation.get(0).getCommand().getValue());
+		Point2D point = new Point2D(newLocation.get(0).getCommand().getValue(), newLocation.get(1).getCommand().getValue());
 		Location loc = new Location(point, 0);
 		data.getTurtle(0).setLocation(loc);
 		return data;
