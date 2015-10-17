c8be5c1ec49733f8c10b6417167045179e6ed6e3
karenli95 <kjl32@duke.edu>
2015-10-17 19:42:07 -0400

    more straightline

--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -5,6 +5,8 @@ import javafx.geometry.Point2D;
 public class Turtle extends SlogoObjects{
 
 	public Turtle() {
-		setTrail(new Trail( new Point2D(0,0), 270 ));
+		Point2D startPoint = new Point2D(0,0);
+		setTrail(new Trail( startPoint, 270 ));
+		this.getTrail().addCoord(startPoint);
 	}
 }
