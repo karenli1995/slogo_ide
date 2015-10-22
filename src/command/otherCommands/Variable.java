6253ac0aac63d0dcc04e4d7bd0260ac962e017c6
sallymerza <sma45@duke.edu>
2015-10-22 00:01:18 -0400

    works

--- src/command/otherCommands/Variable.java
+++ src/command/otherCommands/Variable.java
@@ -5,17 +5,11 @@ import java.util.List;
 import command.Command;
 import command.CommandInterface;
 import controller.ParseTreeNode;
-<<<<<<< HEAD
-=======
-import model.Data;
-import model.MathCommand;
->>>>>>> c7c65261bf1746f61b3a6579db4674bcf7e70bf3
 
-public class Variable extends Command {
+public class Variable  extends Command {
 
 
 	@Override
-<<<<<<< HEAD
 	public double execute(List<ParseTreeNode<CommandInterface>> argument) {
 
 	/*	String varName = argument.get(0).getCommand().getName();
@@ -24,11 +18,6 @@ public class Variable extends Command {
 		System.out.println(data.getVariableMap().size());
 */
 		return 0;
-=======
-	public Data execute(List<ParseTreeNode<Command>> argument, Data data, MathCommand mathcommand) {
-
-		return data;
->>>>>>> c7c65261bf1746f61b3a6579db4674bcf7e70bf3
 	}
 
 
--- src/command/syntax/Constant.java
+++ src/command/syntax/Constant.java
@@ -5,12 +5,7 @@ import java.util.List;
 import command.Command;
 import command.CommandInterface;
 import controller.ParseTreeNode;
-<<<<<<< HEAD
 import model.Data_Turtle_Interface;
-=======
-import model.Data;
-import model.MathCommand;
->>>>>>> c7c65261bf1746f61b3a6579db4674bcf7e70bf3
 
 public class Constant extends Command {
 	Data_Turtle_Interface data;
@@ -24,13 +19,8 @@ public class Constant extends Command {
 	}
 
 	@Override
-<<<<<<< HEAD
 	public double execute(List<ParseTreeNode<CommandInterface>> argument) {
 		return 0;
-=======
-	public Data execute(List<ParseTreeNode<Command>> distance, Data data, MathCommand mathcommand) {
-		return data;
->>>>>>> c7c65261bf1746f61b3a6579db4674bcf7e70bf3
 	}
 
 }
