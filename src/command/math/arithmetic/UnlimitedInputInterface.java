f83674a373d03350199734bc792a6f05f6af03d3
Kevin Wang <kw197@duke.edu>
2015-10-25 13:38:18 -0400

    forgot to add files

---
+++ src/command/math/arithmetic/UnlimitedInputInterface.java
@@ -0,0 +1,8 @@
+package command.math.arithmetic;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface UnlimitedInputInterface {
+    public double doFunction(List<Double> compiledList);
+}
