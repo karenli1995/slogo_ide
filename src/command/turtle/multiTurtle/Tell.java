27c18e6c9e0d8281d13e641ccc263ba8f0803d33
Kevin Wang <kw197@duke.edu>
2015-10-25 22:31:22 -0400

    tell changes

--- src/command/turtle/multiTurtle/Tell.java
+++ src/command/turtle/multiTurtle/Tell.java
@@ -1,11 +1,14 @@
 
 package command.turtle.multiTurtle;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import command.Command;
+import command.CommandInterface;
 import controller.ParseTreeChildren;
+import controller.ParseTreeNode;
 import model.DataTurtleInterface;
+
 
 /**
  *
@@ -13,25 +16,29 @@ import model.DataTurtleInterface;
  *
  */
 public class Tell extends Command {
-	private DataTurtleInterface turtleData;
+    private DataTurtleInterface turtleData;
 
-	public Tell(DataTurtleInterface turtleData) {
-		super(turtleData);
-		this.turtleData = turtleData;
-	}
+    public Tell (DataTurtleInterface turtleData) {
+        super(turtleData);
+        this.turtleData = turtleData;
+    }
 
-	private static final long serialVersionUID = -477042076707708488L;
+    private static final long serialVersionUID = -477042076707708488L;
 
-	@Override
+    @Override
 	public double execute(ParseTreeChildren distance) {
-
-		List<Integer> turtlesToworkOn = null;//kevin what should i put here to get [100] or [1 2 3]
-		int range = turtlesToworkOn.get(0);
-		if (turtlesToworkOn.size() == 1) {
+	    List<ParseTreeNode<CommandInterface>> tempList = distance.getChildListAt(0);
+	    List<Double> turtlesToWorkOn = new ArrayList<Double>();
+	    for(int i = 1; i<tempList.size()-1; i++){
+	        turtlesToWorkOn.add(tempList.get(i).getCommandValue());
+	    }
+	   
+		Double range = turtlesToWorkOn.get(0);
+		if (turtlesToWorkOn.size() == 1) {
 
 			int size = turtleData.turtleListSize();
 			if (range > size) {
-				int diff = range - size;
+				int diff = (int) (range - size);
 				int max = size + diff;
 				for (int i = size; i < max; i++) {
 					turtleData.createTurtle();
@@ -46,10 +53,10 @@ public class Tell extends Command {
 		return range - 1;
 	}
 
-	private void add(int range) {
-		for (int i = 0; i < range; i++) {
-			turtleData.addToActiveList(i);
-		}
-	}
+    private void add (Double range) {
+        for (int i = 0; i < range; i++) {
+            turtleData.addToActiveList(i);
+        }
+    }
 
 }
