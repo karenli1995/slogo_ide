22aaa2545033cf16305391342c4d73ab84f72cc6
karenli95 <kjl32@duke.edu>
2015-10-27 00:24:39 -0400

    MULTIPLE WORKSPACES RESURRECTEDDDDDDDDDDDDDDDDDDDD

--- src/model/data/Data.java
+++ src/model/data/Data.java
@@ -34,10 +34,9 @@ public class Data implements DisplayDataInterface,DataTurtleInterface, ForObserv
 	private String myColorHex;
 	private double myCommandValue;
 	private ActiveTurtles activeTurtles;
-	private static int turtleID = 0;
 	private static int activeTurtle = 0;
-	private static DisplayCommandObserver displayCommands;
-	private static TurtleCommandsObserver turtleCommands;
+	private DisplayCommandObserver displayCommands;
+	private TurtleCommandsObserver turtleCommands;
 
 	public Data() {
 		myTurtles = new ArrayList<SlogoObjects>();
@@ -71,7 +70,6 @@ public class Data implements DisplayDataInterface,DataTurtleInterface, ForObserv
 		myTurtles.add(defaultTurtle);
 		setTurtle(0, defaultTurtle);
 		myTrails.add(defaultTurtle.getTrail());
-//		turtleID++;
 	}
 
 	@Override
@@ -222,9 +220,7 @@ public class Data implements DisplayDataInterface,DataTurtleInterface, ForObserv
 
 	@Override
 	public int getActiveTurtleID() {
-
 		return activeTurtle + 1;
-
 	}
 
 	public Map<String, List<String>> getMyCommandVariableMap() {
@@ -248,8 +244,6 @@ public class Data implements DisplayDataInterface,DataTurtleInterface, ForObserv
 	@Override
 	public void removeElementActiveList(int index) {
 		activeTurtles.remove(index);
-
-
 	}
 
     public Map<String, Double> getMyUserCommandInputMap () {
